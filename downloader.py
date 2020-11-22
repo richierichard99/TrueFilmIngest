c3ca6ca88e360bc3124454f9161727d2bcefcebd
richierichard99 <richierichard99@gmail.com>
2020-11-22 18:28:43 +0000

    renaming setup.py to be more accurate

--- downloader.py
+++ downloader.py
@@ -1,5 +1,4 @@
 import requests
-import sys
 import argparse
 
 """
--- setup.cmd
+++ setup.cmd
@@ -2,7 +2,7 @@
 pip install -r requirements.txt
 
 :: spark dependencies ::
-python setup.py -req maven_requirements.txt
+python downloader.py -req maven_requirements.txt
 
 :: postgres drivers ::
-python setup.py -url https://jdbc.postgresql.org/download/ -req postgres_requirements.txt
+python downloader.py -url https://jdbc.postgresql.org/download/ -req postgres_requirements.txt
