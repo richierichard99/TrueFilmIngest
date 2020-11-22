e961e9d5b971d11b74e713bf40d71b87a08b2cd9
richierichard99 <richierichard99@gmail.com>
2020-11-22 16:14:11 +0000

    improved downloader script to take named and optional args

--- setup.cmd
+++ setup.cmd
@@ -2,4 +2,7 @@
 pip install -r requirements.txt
 
 :: spark dependencies ::
-python setup.py maven_requirements.txt
+python setup.py -req maven_requirements.txt
+
+:: postgres drivers ::
+python setup.py -url https://jdbc.postgresql.org/download/ -req postgres_requirements.txt
